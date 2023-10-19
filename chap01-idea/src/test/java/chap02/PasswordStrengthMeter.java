ed21ddec05b5139109794c535f02de2a3d8feb90
mmihye <smilehye910@naver.com>
2023-10-19 15:08:19 +0900

    [2장] 숫자를 포함하지 않고 나머지 조건은 충족하는 경우 테스트

--- chap01-idea/src/test/java/chap02/PasswordStrengthMeter.java
+++ chap01-idea/src/test/java/chap02/PasswordStrengthMeter.java
@@ -2,9 +2,24 @@ package chap02;
 
 public class PasswordStrengthMeter {
     public PasswordStrength meter(String s){
+
+        //두번째 테스트
         if (s.length() < 8) {
             return PasswordStrength.NORMAL;
         }
+
+        //세번째 테스트
+        boolean containsNum = meetsContainingNumberCriteria(s);
+        if(!containsNum) return PasswordStrength.NORMAL;
         return PasswordStrength.STRONG;
+    }
+
+    private boolean meetsContainingNumberCriteria(String s){
+        for(char ch: s.toCharArray()){
+            if(ch >= '0' && ch <='9'){
+                return true;
+            }
+        }
+        return false;
     }
 }
