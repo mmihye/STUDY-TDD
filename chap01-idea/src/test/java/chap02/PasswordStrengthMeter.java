ac2555d3b9cab6291d00f235f57051d274c3a5b0
mmihye <smilehye910@naver.com>
2023-10-19 15:50:28 +0900

    [2장] 길이가 8글자 이상인 조건만 충족하는 경우 테스트

--- chap01-idea/src/test/java/chap02/PasswordStrengthMeter.java
+++ chap01-idea/src/test/java/chap02/PasswordStrengthMeter.java
@@ -6,19 +6,24 @@ public class PasswordStrengthMeter {
         //네번째 테스트
         if(s==null || s.isEmpty()) return PasswordStrength.INVALID;
 
-        //두번째 테스트
-        if (s.length() < 8) {
-            return PasswordStrength.NORMAL;
-        }
 
+        //두번째 테스트
+//        if (s.length() < 8) {
+//            return PasswordStrength.NORMAL;
+//        }
+
+
+        //여섯번째 테스트 + 두번째 테스트
+        boolean lengthEnough = s.length() >= 8;
         //세번째 테스트
         boolean containsNum = meetsContainingNumberCriteria(s);
-        if(!containsNum) return PasswordStrength.NORMAL;
-
         //다섯번째 테스트
         boolean containsUpp = meetsContainingUppercaseCriteria(s);
-        if(!containsUpp) return PasswordStrength.NORMAL;
 
+        if(lengthEnough && !containsNum && !containsUpp) return  PasswordStrength.WEAK;
+        if(!containsNum) return PasswordStrength.NORMAL;
+        if(!containsUpp) return PasswordStrength.NORMAL;
+        if(!lengthEnough) return PasswordStrength.NORMAL;
 
         return PasswordStrength.STRONG;
     }
