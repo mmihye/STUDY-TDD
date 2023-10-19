c36f0c66e5dcd22248410e2cbed9f8c96e728afd
mmihye <smilehye910@naver.com>
2023-10-19 15:41:43 +0900

    [2장] 대문자를 포함하지 않고 나머지 조건을 충족하는 경우 테스트

--- chap01-idea/src/test/java/chap02/PasswordStrengthMeter.java
+++ chap01-idea/src/test/java/chap02/PasswordStrengthMeter.java
@@ -14,12 +14,27 @@ public class PasswordStrengthMeter {
         //세번째 테스트
         boolean containsNum = meetsContainingNumberCriteria(s);
         if(!containsNum) return PasswordStrength.NORMAL;
+
+        //다섯번째 테스트
+        boolean containsUpp = meetsContainingUppercaseCriteria(s);
+        if(!containsUpp) return PasswordStrength.NORMAL;
+
+
         return PasswordStrength.STRONG;
     }
 
     private boolean meetsContainingNumberCriteria(String s){
         for(char ch: s.toCharArray()){
             if(ch >= '0' && ch <='9'){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean meetsContainingUppercaseCriteria(String s){
+        for(char ch: s.toCharArray()){
+            if(Character.isUpperCase(ch)){
                 return true;
             }
         }
