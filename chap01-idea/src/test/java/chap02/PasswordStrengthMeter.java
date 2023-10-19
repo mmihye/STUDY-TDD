acff80f1b723d59d5c5ec1fbcbc3d95c6e73114d
mmihye <smilehye910@naver.com>
2023-10-19 16:05:23 +0900

    [2장]  아무 조건도 충적하지 않은 경우 테스트

--- chap01-idea/src/test/java/chap02/PasswordStrengthMeter.java
+++ chap01-idea/src/test/java/chap02/PasswordStrengthMeter.java
@@ -10,20 +10,8 @@ public class PasswordStrengthMeter {
         if(meetsContainingNumberCriteria(s)) metCounts ++;
         if(meetsContainingUppercaseCriteria(s)) metCounts ++;
 
-        if(metCounts == 1) return PasswordStrength.WEAK;
+        if(metCounts <= 1) return PasswordStrength.WEAK;
         if(metCounts == 2) return PasswordStrength.NORMAL;
-
-//        boolean lengthEnough = s.length() >= 8;
-//        boolean containsNum = meetsContainingNumberCriteria(s);
-//        boolean containsUpp = meetsContainingUppercaseCriteria(s);
-//
-//        if(lengthEnough && !containsNum && !containsUpp) return  PasswordStrength.WEAK;
-//        if(!lengthEnough && containsNum && !containsUpp) return PasswordStrength.WEAK;
-//        if(!lengthEnough && !containsNum && containsUpp) return PasswordStrength.WEAK;
-//
-//        if(!containsNum) return PasswordStrength.NORMAL;
-//        if(!containsUpp) return PasswordStrength.NORMAL;
-//        if(!lengthEnough) return PasswordStrength.NORMAL;
 
         return PasswordStrength.STRONG;
     }
