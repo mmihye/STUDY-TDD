3ae5e58f2962fe16f1fa845fb60c3eb706885ce9
mmihye <smilehye910@naver.com>
2023-11-02 22:10:42 +0900

    [3장] 다음 쉬운 테스트 코드 (2만원 이상 납부할 경우)

--- chap3-paidService/src/test/java/ExpiryDateCalculatorTest.java
+++ chap3-paidService/src/test/java/ExpiryDateCalculatorTest.java
@@ -72,6 +72,24 @@ public class ExpiryDateCalculatorTest {
         assertExpriyDate(payData3, LocalDate.of(2019,7,31));
     }
 
+    @Test
+    void 이만원_이상_납부하면_비례해서_만료일_계산(){
+        assertExpriyDate(
+                PayData.builder()
+                        .billingDate(LocalDate.of(2019, 3, 1))
+                        .payAmount(20_000)
+                        .build(),
+                LocalDate.of(2019, 5, 1)
+        );
+        assertExpriyDate(
+                PayData.builder()
+                        .billingDate(LocalDate.of(2019, 3, 1))
+                        .payAmount(30_000)
+                        .build(),
+                LocalDate.of(2019, 6, 1)
+        );
+    }
+
     private void assertExpriyDate(
             PayData payData, LocalDate expectedExpriyDate){
         ExpiryDateCalculator cal = new ExpiryDateCalculator();
