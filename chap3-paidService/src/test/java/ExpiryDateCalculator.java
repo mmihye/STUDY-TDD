d19d02b2c9eaa6246e61256695a0d82ac899c372
mmihye <smilehye910@naver.com>
2023-11-02 22:25:54 +0900

    [3장] ExpiryDateCalculator 클래스 코드 정리

--- chap3-paidService/src/test/java/ExpiryDateCalculator.java
+++ chap3-paidService/src/test/java/ExpiryDateCalculator.java
@@ -1,21 +1,30 @@
+import javax.swing.*;
 import java.time.LocalDate;
 import java.time.YearMonth;
 
 public class ExpiryDateCalculator {
-    public LocalDate calculateExpiryDate(PayData payData){
-        int addedMonths =payData.getPayAmount()/10_000;
-        if(payData.getFirstBillingDate() != null){
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
-            if(payData.getFirstBillingDate().getDayOfMonth() !=
-            candidateExp.getDayOfMonth()){
-                if(YearMonth.from(candidateExp).lengthOfMonth()<
-                payData.getFirstBillingDate().getDayOfMonth()){
-                    return candidateExp.withDayOfMonth(
-                            YearMonth.from(candidateExp).lengthOfMonth());
-                }
-                return candidateExp.withDayOfMonth(
-                        payData.getFirstBillingDate().getDayOfMonth());
-            }}
-        return payData.getBillingDate().plusMonths(addedMonths);
+    public LocalDate calculateExpiryDate(PayData payData) {
+        int addedMonths = payData.getPayAmount() / 10_000;
+        if (payData.getFirstBillingDate() != null) {
+            return expiryDateUsingFirstBillingDate(payData, addedMonths);
+        } else {
+            return payData.getBillingDate().plusMonths(addedMonths);
+        }
     }
+
+    private LocalDate expiryDateUsingFirstBillingDate(
+            PayData payData, int addedMonths){
+        LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+        final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+        if(dayOfFirstBilling != candidateExp.getDayOfMonth()){
+            final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
+            if(dayLenOfCandiMon<dayOfFirstBilling){
+                return candidateExp.withDayOfMonth(dayLenOfCandiMon);
+            }
+            return candidateExp.withDayOfMonth(dayOfFirstBilling);
+        }else{
+            return  candidateExp;
+        }
+    }
+
 }
