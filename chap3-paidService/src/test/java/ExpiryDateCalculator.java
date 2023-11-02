3cc4cfcdf8b66250ddfa6d2a4de731ca9ad9aefb
mmihye <smilehye910@naver.com>
2023-11-02 18:07:39 +0900

    [3장] 가장 쉬운 경우 테스트코드 작성 ( 1만원을 납부하면 만료일이 한달 뒤)

---
+++ chap3-paidService/src/test/java/ExpiryDateCalculator.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+
+public class ExpiryDateCalculator {
+    public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount){
+        return LocalDate.of(2019, 4, 1);
+    }
+}
